167ac5e2c6743c45cd3cc5939c70a673007728cc
Kyle Krafka <kyle.krafka@gmail.com>
2013-08-29 00:21:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Twitmote
+========
+
+Turn your Twitter account into a remote control service for your computer!
+
+About
+-----
+Twitmote is a service that runs on your computer and periodically checks for Direct Messages on a Twitter account of your choice.  If the Direct Message meets one of the criteria you set up, any command can be triggered on your computer.
+
+This was originally a Perl project called "KTRC: Kyle's Twitter Remote Control."  I wrote it back in December of 2008 before I had a data plan on my phone but still wanted to stay connected.  I knew Twitter had a pretty solid (and free!) text message interface and API, so I realized I could connect my phone to my computer (and thus, the Internet) with just a bit of work!  The first milestone was the ability to play and pause Winamp via text.  Later, I got Halo 3 stats delivered via a return Direct Message.
+
+Now the project has been rewritten in Python and I use it from time-to-time on my Mac to execute anything I would normally type into Terminal.  Perhaps my favorite command is Apple's text-to-speech command, "say."  I have a Twitter account that I use exclusively for this purpose, and because only those that I follow can Direct Message me, I don't need to worry about malicious messages.  (Additional security can be implemented on the server.)  I just have my bot set to follow me.
+
+Important Note
+--------------
+As of June 2010, this no longer works due to Twitter's API switching to OAuth for authentication.  Pull requests implementing a fix are welcome!
---
+++ twitmote.py
@@ -0,0 +1,72 @@
+#! /usr/bin/env python
+# Twitmote
+# A remote control server for Twitter Direct Messages
+# File created: 2009-05-09
+# Author: Kyle Krafka
+
+import getpass
+import base64
+import string # TODO: from string import strip
+import httplib
+from xml.dom import minidom
+from os import system
+import time
+
+print "Greetings!  Please log your Twitter Bot in."
+
+while True:
+    username = raw_input("Username: ")
+    password = getpass.getpass("Password: ")
+
+    auth = 'Basic ' + \
+    string.strip(base64.encodestring(username + ":" + password))
+    conn = httplib.HTTPConnection('twitter.com')
+    conn.putrequest('GET', '/direct_messages.xml?count=1')
+    conn.putheader('Authorization', auth)
+    conn.endheaders()
+    resp = conn.getresponse()
+    if resp.status == 200:
+        print "You're good.\n"
+        break
+    else:
+        print 'Your credentials could not be verified ("'+\
+        `resp.status`+' '+\
+        resp.reason+'").  Please try again.\n'
+
+# Read the data into a string
+data = resp.read()
+dom = minidom.parseString(data)
+since_id = dom.getElementsByTagName('direct_message')[0]\
+.getElementsByTagName('id')[0].firstChild.nodeValue
+loop_count = 0
+
+# The initial since_id has been set, begin the regular message checking.
+
+while True:
+    conn.putrequest('GET', '/direct_messages.xml?since_id='+since_id)
+    conn.putheader('Authorization', auth)
+    conn.endheaders()
+    resp = conn.getresponse()
+    if resp.status == 200:
+	data = resp.read()
+	dom = minidom.parseString(data)
+	# TODO: do this list in reverse
+	textNodesReversed = dom.getElementsByTagName('text')
+	textNodesReversed.reverse()
+	for command in textNodesReversed:
+	    command = command.firstChild.nodeValue
+	    print "Command received:", command
+        # Put logic for processing the command here, or directly interpret it
+        system(command)
+	# if last time, set new since_id
+	if len(textNodesReversed) > 0: # if there were any new commands
+	    since_id = dom.getElementsByTagName('direct_message')[0]\
+	    .getElementsByTagName('id')[0].firstChild.nodeValue
+	loop_count = loop_count + 1
+	print "Just checked: ", loop_count
+	time.sleep(36)
+	# check wait limit
+    else:
+        print 'There was a problem connecting ("'+\
+        `resp.status`+' '+\
+        resp.reason+'").  Will continue to try.\n'
